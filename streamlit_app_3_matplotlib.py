0616cc1f5f82613646d3b498953f1a043abd2d8f
Brian Spiering <bspiering@gmail.com>
2021-06-29 12:45:38 -0700

    Updates

--- streamlit_app_3_matplotlib.py
+++ streamlit_app_3_matplotlib.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 st.title("Simulation")
 st.write("'We have to realize that computers are simulators and then figure out what to simulate.' \n\n — Alan Kay")
 
-trend = st.slider('Trend',  min_value=0.001, max_value=0.10, step=0.01)
-noise = st.slider('Noise',min_value=0.01,  max_value=0.10, step=0.01)
+trend = st.slider('Trend', min_value=0.001, max_value=0.10, step=0.01)
+noise = st.slider('Noise', min_value=0.01,  max_value=0.10, step=0.01)
 st.write(f"Trend = {trend} \n\n Noise = {noise}")
 
 intial_value = 1
