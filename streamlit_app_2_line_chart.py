6caa17bf32f3f133384c631e73aef94372a1e125
Brian Spiering <bspiering@gmail.com>
2021-06-29 12:11:31 -0700

    Updates

--- streamlit_app_2_line_chart.py
+++ streamlit_app_2_line_chart.py
@@ -1,14 +1,19 @@
 """
-Example of a straightforward Streamlit app
+Example of a line chart of time-series simulation
 """
 
 import streamlit as st
-
+import numpy as np
 
 st.title("Simulation")
 st.write("'We have to realize that computers are simulators and then figure out what to simulate.' \n\n — Alan Kay")
 
 rate = st.slider('Rate',  min_value=0.001, max_value=0.10, step=0.01)
 noise = st.slider('Noise',min_value=0.01,  max_value=0.10, step=0.01)
-
 st.write(f"Rate = {rate} \n\n Noise = {noise}")
+
+intial_value = 1
+n_series = 10 
+time_series = np.cumprod(intial_value + np.random.normal(rate, noise, (100, n_series)), 
+                         axis=0)
+st.line_chart(time_series)
