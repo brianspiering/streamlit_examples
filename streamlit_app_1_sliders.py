9f9275ff0cde9aed24069fb9fc0465276d902bc1
Brian Spiering <bspiering@gmail.com>
2021-06-29 10:51:42 -0700

    Updates

--- streamlit_app_1_sliders.py
+++ streamlit_app_1_sliders.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 
 st.title("Simulation")
-st.write("We have to realize that computers are simulators and then figure out what to simulate. — Alan Kay")
+st.write("We have to realize that computers are simulators and then figure out what to simulate. \n — Alan Kay")
 
 intercept = st.slider('Intercept', min_value=1, max_value=100, step=1)
 slope = st.slider('Slope', min_value=1, max_value=100, step=1)
