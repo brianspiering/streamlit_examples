7fffb47e5fe95e7fe83afa90c615fbaa3cea4e9f
Brian Spiering <bspiering@gmail.com>
2021-06-30 10:29:52 -0700

    Updates

--- streamlit_app_4_airline_passenger_starter_code.py
+++ streamlit_app_4_airline_passenger_starter_code.py
@@ -11,18 +11,20 @@ import pandas as pd
 # TODO: Add a title for the web app
 
 
-# TODO: Add A short piece of text describing what the web app does
+# TODO: Add a short text description what the web app does
 
 
 # Load data is done for you
 url = "https://raw.githubusercontent.com/brianspiering/datasets/main/airline_passenger_data.csv"
 df = pd.read_csv(url)
 
-# TODO: Create sliders for the stop and start indices
+# TODO: Create sliders for start index and number of months
+start_index = st.slider('Start index',  min_value=0, max_value=df.shape[0], step=1)
+n_months  = st.slider('Number of months',  min_value=2,  max_value=df.shape[0]-start_index, step=1)
 
 
-# Select out just the rows and single column to plot is done for you.
-data = df.iloc[start_index:stop_index, 2]
+# Select just the rows and single column to plot
+data = df.iloc[start_index:start_index+n_months, 2]]
 
 # TODO: Create a line chart of the data
 
